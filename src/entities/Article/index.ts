918bc60333dcd3f98617277b227df1a34844c2cd
Irina Arinushkina <ira.arinushkina@gmail.com>
2023-05-16 21:47:19 +0300

    add circular dependency plugin

--- src/entities/Article/index.ts
+++ src/entities/Article/index.ts
@@ -10,10 +10,7 @@ export {
   ArticleSortSelector,
 } from './ui/ArticleSortSelector/ArticleSortSelector';
 
-export {
-  type Article,
-
-} from './model/types/article';
+export type { Article } from './model/types/article';
 export type { ArticleDetailsSchema } from './model/types/articleDetailsSchema';
 
 export { getArticleDetailsData } from './model/selectors/articleDetails';
