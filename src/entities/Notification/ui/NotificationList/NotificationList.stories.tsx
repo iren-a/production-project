a99caebccf53dcccf3e1d05c06b800e80f2cd311
Irina Arinushkina <ira.arinushkina@gmail.com>
2023-06-02 17:19:14 +0300

    fix storybook, add mocks for notification list, article rating

--- src/entities/Notification/ui/NotificationList/NotificationList.stories.tsx
+++ src/entities/Notification/ui/NotificationList/NotificationList.stories.tsx
@@ -1,16 +1,44 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+import withMock from 'storybook-addon-mock';
 import { NotificationList } from './NotificationList';
 
 export default {
-  title: 'shared/NotificationList',
+  title: 'entities/Notification/NotificationList',
   component: NotificationList,
   argTypes: {
     backgroundColor: { control: 'color' },
   },
+  decorators: [withMock],
 } as ComponentMeta<typeof NotificationList>;
 
 const Template: ComponentStory<typeof NotificationList> = (args) => <NotificationList {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {};
+Primary.parameters = {
+  mockData: [
+    {
+      url: `${__API__}/notifications`,
+      method: 'GET',
+      status: 200,
+      response: [
+        {
+          id: '1',
+          title: 'Уведомление 1',
+          description: 'Текст уведомления',
+        },
+        {
+          id: '2',
+          title: 'Уведомление 2',
+          description: 'Текст уведомления',
+        },
+        {
+          id: '3',
+          title: 'Уведомление 3',
+          description: 'Текст уведомления',
+        },
+      ],
+    },
+  ],
+};
