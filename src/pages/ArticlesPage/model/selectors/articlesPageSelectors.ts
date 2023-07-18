bab6d9947b5a50969b05280032c0a45258f9d168
Irina Arinushkina <ira.arinushkina@gmail.com>
2023-07-18 14:58:38 +0300

    add arguments for selector

--- src/pages/ArticlesPage/model/selectors/articlesPageSelectors.ts
+++ src/pages/ArticlesPage/model/selectors/articlesPageSelectors.ts
@@ -1,25 +1,23 @@
 import { StateSchema } from '@/app/providers/StoreProvider';
 import { ArticleSortField, ArticleType, ArticleView } from '@/entities/Article';
+import { buildSelector } from '@/shared/lib/store';
 
 export const getArticlesPageIsLoading = (state: StateSchema) =>
   state.articlesPage?.isLoading || false;
 export const getArticlesPageView = (state: StateSchema) =>
   state.articlesPage?.view || ArticleView.SMALL;
-export const getArticlesPageError = (state: StateSchema) =>
-  state.articlesPage?.error;
-export const getArticlesPageNum = (state: StateSchema) =>
-  state.articlesPage?.page || 1;
-export const getArticlesPageLimit = (state: StateSchema) =>
-  state.articlesPage?.limit || 9;
-export const getArticlesPageHasMore = (state: StateSchema) =>
-  state.articlesPage?.hasMore;
-export const getArticlesPageInitialized = (state: StateSchema) =>
-  state.articlesPage?._initialized;
+export const getArticlesPageError = (state: StateSchema) => state.articlesPage?.error;
+export const getArticlesPageNum = (state: StateSchema) => state.articlesPage?.page || 1;
+export const getArticlesPageLimit = (state: StateSchema) => state.articlesPage?.limit || 9;
+export const getArticlesPageHasMore = (state: StateSchema) => state.articlesPage?.hasMore;
+export const getArticlesPageInitialized = (state: StateSchema) => state.articlesPage?._initialized;
 export const getArticlesPageSort = (state: StateSchema) =>
   state.articlesPage?.sort ?? ArticleSortField.CREATED;
-export const getArticlesPageOrder = (state: StateSchema) =>
-  state.articlesPage?.order ?? 'desc';
-export const getArticlesPageSearch = (state: StateSchema) =>
-  state.articlesPage?.search ?? '';
+export const getArticlesPageOrder = (state: StateSchema) => state.articlesPage?.order ?? 'desc';
+export const getArticlesPageSearch = (state: StateSchema) => state.articlesPage?.search ?? '';
 export const getArticlesPageType = (state: StateSchema) =>
   state.articlesPage?.type ?? ArticleType.ALL;
+
+export const [useArticleItemById] = buildSelector(
+  (state, id: string) => state.articlesPage?.entities[id],
+);
