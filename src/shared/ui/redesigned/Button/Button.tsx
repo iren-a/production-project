858c432f1fcd07be174a06cbbb419178eae5a948
Irina Arinushkina <ira.arinushkina@gmail.com>
2023-08-22 13:29:17 +0300

    fix button in listbox

--- src/shared/ui/redesigned/Button/Button.tsx
+++ src/shared/ui/redesigned/Button/Button.tsx
@@ -1,4 +1,4 @@
-import { ButtonHTMLAttributes, memo, ReactNode } from 'react';
+import { ButtonHTMLAttributes, ForwardedRef, forwardRef, ReactNode } from 'react';
 import { classNames, Mods } from '@/shared/lib/classNames/classNames';
 import cls from './Button.module.scss';
 
@@ -42,7 +42,7 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   addonRight?: ReactNode;
 }
 
-export const Button = memo((props: ButtonProps) => {
+export const Button = forwardRef((props: ButtonProps, ref: ForwardedRef<HTMLButtonElement>) => {
   const {
     className,
     children,
@@ -69,6 +69,7 @@ export const Button = memo((props: ButtonProps) => {
       type="button"
       className={classNames(cls.Button, mods, [className, cls[variant], cls[color], cls[size]])}
       disabled={disabled}
+      ref={ref}
       {...otherProps}
     >
       {addonLeft && <div className={cls.addonLeft}>{addonLeft}</div>}
