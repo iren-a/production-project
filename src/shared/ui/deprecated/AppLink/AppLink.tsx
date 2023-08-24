7b654c912ac8a2d81df1b90fcda65df8ff9c25d5
Irina Arinushkina <ira.arinushkina@gmail.com>
2023-08-24 11:36:53 +0300

    fix app link

--- src/shared/ui/deprecated/AppLink/AppLink.tsx
+++ src/shared/ui/deprecated/AppLink/AppLink.tsx
@@ -1,4 +1,4 @@
-import { memo } from 'react';
+import { ForwardedRef, forwardRef } from 'react';
 import { Link, LinkProps } from 'react-router-dom';
 import cls from './AppLink.module.scss';
 import { classNames } from '@/shared/lib/classNames/classNames';
@@ -17,11 +17,16 @@ interface AppLinkProps extends LinkProps {
  * @deprecated
  * Устарел, используем новые компоненты из папки redesigned
  */
-export const AppLink = memo((props: AppLinkProps) => {
+export const AppLink = forwardRef((props: AppLinkProps, ref: ForwardedRef<HTMLAnchorElement>) => {
   const { to, className, children, theme = AppLinkTheme.Primary, ...otherProps } = props;
 
   return (
-    <Link to={to} className={classNames(cls.AppLink, {}, [className, cls[theme]])} {...otherProps}>
+    <Link
+      to={to}
+      ref={ref}
+      className={classNames(cls.AppLink, {}, [className, cls[theme]])}
+      {...otherProps}
+    >
       {children}
     </Link>
   );
--- src/shared/ui/redesigned/AppLink/AppLink.tsx
+++ src/shared/ui/redesigned/AppLink/AppLink.tsx
@@ -1,4 +1,4 @@
-import { memo } from 'react';
+import { ForwardedRef, forwardRef } from 'react';
 import { LinkProps, NavLink } from 'react-router-dom';
 import cls from './AppLink.module.scss';
 import { classNames } from '@/shared/lib/classNames/classNames';
@@ -8,12 +8,13 @@ interface AppLinkProps extends LinkProps {
   activeClassName?: string;
 }
 
-export const AppLink = memo((props: AppLinkProps) => {
+export const AppLink = forwardRef((props: AppLinkProps, ref: ForwardedRef<HTMLAnchorElement>) => {
   const { to, className, activeClassName = '', children, ...otherProps } = props;
 
   return (
     <NavLink
       to={to}
+      ref={ref}
       className={({ isActive }) =>
         classNames(cls.AppLink, { [activeClassName]: isActive }, [className])
       }
